a890fa9d2421ab8a4e3d720f998d81acb0d256cb
James Wyatt Cready-Pyle <jcready@uber.com>
2019-12-04 08:43:04 -0500

    Update corners.js

--- src/util/corners.js
+++ src/util/corners.js
@@ -6,6 +6,10 @@ export default function corners(nw, ne, se, sw) {
       if (arg.ne || arg.ne === 0) { target[ne] = arg.ne; }
       if (arg.se || arg.se === 0) { target[se] = arg.se; }
       if (arg.sw || arg.sw === 0) { target[sw] = arg.sw; }
+      if (arg.topLeft || arg.topLeft === 0) { target[nw] = arg.topLeft; }
+      if (arg.topRight || arg.topRight === 0) { target[ne] = arg.topRight; }
+      if (arg.bottomRight || arg.bottomRight === 0) { target[se] = arg.bottomRight; }
+      if (arg.bottomLeft || arg.bottomLeft === 0) { target[sw] = arg.bottomLeft; }
       return target;
     }
     target[nw] = arg;
