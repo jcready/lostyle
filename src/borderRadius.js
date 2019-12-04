fe292e0d48797143e8ff1bda180c430af51dbb82
James Wyatt Cready-Pyle <jcready@uber.com>
2019-12-04 08:36:59 -0500

    Update borderRadius.js

--- src/borderRadius.js
+++ src/borderRadius.js
@@ -1,44 +1,45 @@
-import directional from './util/directional.js';
+import corners from './util/corners.js';
 
 /**
  * Directional borderRadius helper
  * @function borderRadius
- * @param  {number|string|object} value         Value for all directions or object of values:
- * @param  {number|string}       [value.x]      Left and right value
- * @param  {number|string}       [value.y]      Top and bottom value
- * @param  {number|string}       [value.top]    Top value (takes precedence over y)
- * @param  {number|string}       [value.right]  Right value (takes precedence over x)
- * @param  {number|string}       [value.bottom] Bottom value (takes precedence over y)
- * @param  {number|string}       [value.left]   Left value (takes precedence over x)
+ * @param  {number|string|object} value              Value for all directions or object of values:
+ * @param  {number|string}       [value.nw]          North West value ╭
+ * @param  {number|string}       [value.ne]          North East value ╮
+ * @param  {number|string}       [value.se]          South East value ╯
+ * @param  {number|string}       [value.sw]          South West value ╰
+ * @param  {number|string}       [value.topLeft]     Top Left value (takes precedence over nw)
+ * @param  {number|string}       [value.topRight]    Top Right value (takes precedence over ne)
+ * @param  {number|string}       [value.bottomRight] Bottom Right value (takes precedence over se)
+ * @param  {number|string}       [value.bottomLeft]  Bottom Left value (takes precedence over sw)
  * @return {object}                             Style object
  * @example
  * // All directions
  * borderRadius('foo');
  * // → {
- * //     borderTopRadius: 'foo',
- * //     borderRightRadius: 'foo',
- * //     borderBottomRadius: 'foo',
- * //     borderLeftRadius: 'foo'
+ * //     borderTopLeftRadius: 'foo',
+ * //     borderTopRightRadius: 'foo',
+ * //     borderBottomRightRadius: 'foo',
+ * //     borderBottomLeftRadius: 'foo'
  * //   }
  * @example
- * // A single axis
- * borderRadius({x: 'bar'});
+ * // Using compass directions
+ * borderRadius({nw: 'bar', se: 'baz'});
  * // → {
- * //     borderLeftRadius: 'bar',
- * //     borderRightRadius: 'bar'
+ * //     borderTopLeftRadius: 'bar',
+ * //     borderBottomRightRadius: 'bar'
  * //   }
  * @example
- * // Any combination
- * borderRadius({y: 'foo', top: 'bar', right: 'baz'});
+ * // Using box directions
+ * borderRadius({topRight: 'bar', bottomLeft: 'baz'});
  * // → {
- * //     borderTopRadius: 'bar',
- * //     borderRightRadius: 'baz',
- * //     borderBottomRadius: 'foo'
+ * //     borderTopRightRadius: 'bar',
+ * //     borderBottomLeftRadius: 'baz'
  * //   }
  */
 export default directional(
-  'borderTopRadius',
-  'borderRightRadius',
-  'borderBottomRadius',
-  'borderLeftRadius'
+  'borderTopLeftRadius',
+  'borderTopRightRadius',
+  'borderBottomRightRadius',
+  'borderBottomLeftRadius'
 );
